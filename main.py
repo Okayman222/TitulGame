219c0de6cecaee409900b876a648d4b9af449483
Okayman222 <133891031+Okayman222@users.noreply.github.com>
2023-05-31 20:04:35 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 import pygame
 import math
+import time
+
+bullets = pygame.sprite.Group()
+
+us = 1
 
 camera_x = 0
 camera_y = 0
@@ -17,8 +22,9 @@ FPS = 60
 col = math.ceil(win_width / bg_width) + 1
 
 
-isjump = False
-jump = 10
+
+
+
 
 scroll = 0
 
@@ -31,41 +37,72 @@ class GameSprite(pygame.sprite.Sprite):
         self.rect = self.image.get_rect()
         self.rect.x = player_x
         self.rect.y = player_y
+        self.jump = 10
+        self.list = ['d.png','d2.png']
+        self.isjump = False
+        self.us = 1
 
 
+    def move2(self):
+        self.rect.x -= 1
 
     def reset(self):
         window.blit(self.image, (self.rect.x, self.rect.y))
 
+
+class Bullet(GameSprite):
+    def move4(self):
+        self.rect.x -= self.speed
+
+    def move3(self):
+        self.rect.x += self.speed
+
+class Hero(GameSprite):
     def update(self):
         keys = pygame.key.get_pressed()
-        if keys[pygame.K_LEFT] and self.rect.x > 5:
-            self.rect.x -= 10
-        if keys[pygame.K_RIGHT] and self.rect.x < win_width - 80:
-            self.rect.x += 10
+        if keys[pygame.K_LEFT] and hero.rect.x > 5:
+            hero.rect.x -= 10
+            hero.image = pygame.transform.scale(pygame.image.load('d2.png'), (150, 100))
+            self.us = 2
 
-        if not (isjump):
+        if keys[pygame.K_RIGHT] and hero.rect.x < win_width - 80:
+            hero.rect.x += 10
+            hero.image = pygame.transform.scale(pygame.image.load('d.png'), (150, 100))
+            self.us = 1
+        if not (self.isjump):
             if keys[pygame.K_LEFT] and self.rect.x > 5:
                 self.rect.x -= 10
-
             if keys[pygame.K_RIGHT] and self.rect.x < win_width - 80:
                 self.rect.x += 10
-            if keys[pygame.K_SPACE]:
-                if jump >= -10:
-                    if jump < 0:
-                        self.rect.y += (jump ** 2) / 4
+            if keys[pygame.K_UP]:
+                self.isjump = True
+        else:
+            if not (self.isjump):
+                if keys[pygame.K_LEFT] and self.rect.x > 5:
+                    self.rect.x -= 10
+                if keys[pygame.K_RIGHT] and self.rect.x < win_width - 80:
+                    self.rect.x += 10
+                if keys[pygame.K_UP]:
+                    self.isjump = True
+            else:
+                if self.jump >= -10:
+                    if self.jump <= 0:
+                        self.rect.y += (self.jump ** 2) / 2
+
                     else:
-                        self.rect.y -= (jump ** 2) / 4
-                    jump -= 1
+                        self.rect.y -= (self.jump ** 2) / 2
+                    self.jump -= 1
                 else:
-                    isjumps = True
-                    jump = 10
+                    self.isjump = False
+                    self.jump = 10
+
+    def fire(self):
+        bullet = Bullet('ullet.png', self.rect.centerx,  self.rect.centery, 15, 20, 15)
+        bullets.add(bullet)
 
 
-
-
-
-hero = GameSprite('d.png', 20, win_height - 100, 150 ,100 ,4)
+monster = GameSprite('m1.png',1920, win_height - 85, 70 ,90 ,4)
+hero = Hero('d.png', 20, win_height - 101, 150 ,100 ,4)
 
 game = True
 
@@ -73,14 +110,32 @@ while game:
     for e in pygame.event.get():
         if e.type == pygame.QUIT:
             game = False
+        elif e.type == pygame.KEYDOWN:
+            if e.key == pygame.K_SPACE:
+                hero.fire()
+
+
+
     for i in range(0,col):
         window.blit(bg,(i*bg_width + scroll,0))
 
-    scroll -= 1
+    bullets.draw(window)
+    if hero.us == 2:
+        for i in bullets:
+            i.move4()
+    elif hero.us == 1:
+        for i in bullets:
+            i.move3()
+
+
+
+    scroll -=2
 
     if scroll*-1 > bg_width:
         scroll = 0
 
+    monster.reset()
+    monster.move2()
     hero.reset()
     hero.update()
 
